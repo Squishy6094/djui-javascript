5effe9d77403b31a234dc58b0637a5aa8ba89d66
Squishy <squishy6094@gmail.com>
2025-06-26 17:30:54 -0700

    async mind virus spreads

--- djui.js
+++ djui.js
@@ -232,7 +232,7 @@ function djui_hud_print_text(text, x, y, scale) {
     context.fillText(text, x * resScale, y * resScale);
 }
 
-function get_texture_info(texName) {
+async function get_texture_info(texName) {
     const img = new Image();
     img.src = texName
     return img;
